36fa6276893517399c0bd21f9beec892c1aab770
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-02 00:14:50 -0300

    feat: primeiras regras de negocio para entidade usuario

---
+++ src/Domain/Entities/Usuario.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Text.RegularExpressions;
+namespace Entities
+{
+    public class Usuario
+    {
+        public int Id_usuario {get; private set;}
+        public string Nome {get; private set;}
+        public string Email {get; private set;}
+        public string Senha_hash {get; private set;}
+        public bool Ativo {get; private set;}
+        public DateTime Data_Criacao {get; private set;}
+        public DateTime? Ultimo_login {get; private set;}
+
+
+        protected Usuario(){}
+
+
+        public Usuario(string nome, string email, string senhaHash)
+        {
+            if(string.IsNullOrWhiteSpace(nome))
+                throw new ArgumentException("Nome é obirgatório");
+            if(string.IsNullOrWhiteSpace(email))
+                throw new ArgumentException("Email é obirgatório");
+
+            
+            Nome = nome;
+            Email = email;
+            Senha_hash = senhaHash;
+            Ativo = true;
+            Data_Criacao = DateTime.UtcNow;
+        }
+
+
+        public void AtualizarUltimoLogin()
+        {
+            Ultimo_login = DateTime.UtcNow;
+        }
+
+        public void Desativar()
+        {
+            Ativo = false;
+        }
+
+        public void Ativar()
+        {
+            Ativo = true;
+        }
+
+    }
+
+}
