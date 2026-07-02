ae5fcbd652556f1b92bb315e0e3d895e5110698e
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-02 16:13:28 -0300

    feat/logica interna da entidade Permissao

---
+++ src/Domain/Entities/Permissao.cs
@@ -0,0 +1,42 @@
+using Domain.Exceptions;
+
+namespace Domain.Entities;
+
+public class Permissao
+{
+    public int IdPermissao {get; private set;}
+    public string NomePermissao {get; private set;}
+    public string? Descricao {get; private set;}
+
+    protected Permissao(){} 
+
+    public Permissao(string nomePermissao, string? descricao = null)
+    {
+        ValidarNomePermissao(nomePermissao);
+
+        NomePermissao = nomePermissao;
+        Descricao = descricao;
+    }
+
+    public void Atualizar(string nomePermissao, string? descricao)
+    {
+        ValidarNomePermissao(nomePermissao);
+
+        NomePermissao = nomePermissao;
+        Descricao = descricao;
+    }
+
+
+    private void ValidarNomePermissao(string nomePermissao)
+    {
+        if(string.IsNullOrWhiteSpace(nomePermissao))
+            throw new DomainException("Nome e obirgatorio");
+
+        if(nomePermissao.Trim().Length < 3)
+            throw new DomainException("Nome da permissao deve ter no minimo 3 caracteres");
+
+        if(nomePermissao.Trim().Length > 100)
+            throw new DomainException("Nome da permissao deve ter no maximo 100 caracteres");
+    }
+
+}
