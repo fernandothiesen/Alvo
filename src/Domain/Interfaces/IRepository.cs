1f0b6cf72909750814e1fe45fe06d98a3260750a
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-03 12:01:13 -0300

    feat/ Interfaces para repository Genereica

---
+++ src/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+using Entities;
+
+namespace Domain.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<T?> ObterPorIdAsync(int id);
+    Task<IEnumerable<T>> ObterTodosAsync();
+    Task AdicionarAsync(T entidade);
+    Task AtualizarAsync(T entidade);
+    Task RemoverAsync(int id);
+}
