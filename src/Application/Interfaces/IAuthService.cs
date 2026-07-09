c9d9b31aae71486949fea7dc05d8c7ab55b3f268
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-09 09:12:17 -0300

    feat(Application): interface para contrato de hash senha

--- src/Application/Interfaces/IAuthService.cs
+++ src/Application/Interfaces/IAuthService.cs
@@ -2,7 +2,7 @@ using Application.DTOs.Auth;
 using Application.DTOs.Response;
 using Application.DTOs.Usuario;
 
-namespace Application.DTOs.Interfaces;
+namespace Application.Interfaces;
 public interface IAuthService
 {
     Task<ResponseResult> LoginAsync(LoginDto dto);
