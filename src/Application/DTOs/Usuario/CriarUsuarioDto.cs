331b98b5ba1713b309176a6a99bc5d368d45e044
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-09 08:19:43 -0300

    feat(DTO): Dto de criacao de usuario

--- src/Application/DTOs/Usuario/CriarUsuarioDto.cs
+++ src/Application/DTOs/Usuario/CriarUsuarioDto.cs
@@ -1,17 +1,30 @@
 using System.ComponentModel.DataAnnotations;
-using Domain.Entities;
+using System.Diagnostics.CodeAnalysis;
+
 
 
 namespace Application.DTOs.Usuario;
 
 public class CriarUsuarioDto
 {
+    [Required(ErrorMessage = "O nome e obrigatorio")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome deve ter entre 3 e 100 caracteres")]
+    public string Nome {get; set;} = string.Empty;
 
-    [Required, MaxLength(100)]
-    public string Nome {get; set;}
+    [Required(ErrorMessage = "Email e obrigatorio")]
+    [EmailAddress(ErrorMessage = "O email nao e valido")]
+    public string Email {get; set;} = string.Empty;
 
-    [Required, MaxLength(150)]
-    public string Email {get; set;}
-    [Required]
-    public string senha {get; set;}
+    [Required(ErrorMessage = "Senha e obrigatorio")]
+    [MinLength(6, ErrorMessage = "Senha deve ter no minimo 6 caracteres")]
+    public string Senha {get; set;} = string.Empty;
+
+    [Required(ErrorMessage = "Confirmar senha e obrigatorio")]
+    [Compare("Senha", ErrorMessage = "As senhas nao coincidem")]
+    public string ConfirmarSenha {get; set;} = string.Empty;
+
+
+    //Roles opcionais na criacao 
+    public List<int> IdsRoles {get; set;} = new();
+
 }
