4ffb71915f4fd89a2e2b8bda4e1eb3934903c4ff
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-09 09:22:59 -0300

    feat(application): Interface de servico de usuario

---
+++ src/Application/Interfaces/IUsuarioService.cs
@@ -0,0 +1,12 @@
+using Application.DTOs.Usuario;
+using Application.DTOs.Response;
+
+
+public interface IUsuarioService
+{
+    Task<ResponseResult> CriarUsuarioAsync (CriarUsuarioDto dto);
+    Task<UsuarioDto?> ObterPorId (int id);
+    Task<IEnumerable<UsuarioDto>> ObterTodosAsync();
+    Task<ResponseResult> AtualizarAsync(int id, CriarUsuarioDto dto);
+    Task<ResponseResult> DesativarAsync(int id);
+}
---
+++ src/Application/Services/UsuarioService.cs
@@ -0,0 +1,10 @@
+using Application.DTOs.Usuario;
+using Application.DTOs.Response;
+using Application.Interfaces;
+using Domain.Entities;
+using Domain.Exceptions;
+using Domain.Interfaces;
+
+
+
+namespace Application.Services;
