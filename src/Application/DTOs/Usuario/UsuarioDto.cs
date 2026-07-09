d815ace20208604420ae4331ceff290ee448978a
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-09 08:24:32 -0300

    feat(DTO): DTOs de Usuario

---
+++ src/Application/DTOs/Usuario/UsuarioDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Usuario;
+
+public class UsuarioDto
+{
+    public int IdUsuario {get; set;}
+    public string Nome {get; set;} = string.Empty;
+    public string Email {get; set;} = string.Empty;
+    public bool Ativo {get; set;}
+    public DateTime DataCriacao {get; set;}
+    public DateTime? UltimoLogin {get; set;}
+    List<string> Roles {get; set;} = new();
+}
