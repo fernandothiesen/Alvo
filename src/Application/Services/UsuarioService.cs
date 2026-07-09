a939af66a8b3b32907532a625e5a8bfdf6071355
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-09 09:39:21 -0300

    refactor/Interface de usuario repository na domain estava faltando de add, remover e att

--- src/Application/Services/UsuarioService.cs
+++ src/Application/Services/UsuarioService.cs
@@ -8,3 +8,42 @@ using Domain.Interfaces;
 
 
 namespace Application.Services;
+
+
+
+public class UsuarioService : IUsuarioService
+{
+    
+    private readonly IUsuario _usuarioRepository;
+    private readonly IPasswordHasher _passwordHasher;
+
+
+    public UsuarioService(IUsuario usuarioRepository, IPasswordHasher passwordHasher)
+    {
+        _usuarioRepository = usuarioRepository;
+        _passwordHasher = passwordHasher;
+    }
+
+    public async Task<ResponseResult> CriarUsuarioAsync(CriarUsuarioDto dto)
+    {
+        try
+        {
+            if(await _usuarioRepository.EmailExistenteAsync(dto.Email))
+                return ResponseResult.Erro("Ja existe um Usuario com este email");
+
+            //(usando a interface, mas infrastructure vai usar BCrypt)
+            var senhaHash = _passwordHasher.HashPassword(dto.Senha);
+
+            var usuario = new Usuario(dto.Nome, dto.Email, senhaHash);
+
+            await _usuarioRepository.AdicionarAsync(usuario);
+
+
+        }
+
+
+    }
+
+
+
+}
--- src/Domain/Interfaces/IUsuarioRepository.cs
+++ src/Domain/Interfaces/IUsuarioRepository.cs
@@ -7,4 +7,7 @@ public interface IUsuario : IRepository<Cliente>
     Task<Usuario?> ObterPorEmailAsync(string email);
     Task<IEnumerable<Usuario>> ObterAtivosAsync();
     Task<bool> EmailExistenteAsync(string email, int? idUsuarioExcluir = null);
+    Task AdicionarAsync (Usuario usuario);
+    Task AtualizarAsync (Usuario usuario);
+    Task RemoverAsync(int idUsuario);
 }
