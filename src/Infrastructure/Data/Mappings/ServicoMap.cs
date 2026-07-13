9fba542ca4dd483b74209945e6177d196c9243d2
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-13 15:10:45 -0300

    fix: mapping de Servico estava usando de Pais

--- src/Infrastructure/Data/Mappings/ServicoMap.cs
+++ src/Infrastructure/Data/Mappings/ServicoMap.cs
@@ -2,21 +2,26 @@ using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-
-
 namespace Infrastructure.Data.Mappings;
-
 
 public class ServicoMap : IEntityTypeConfiguration<Servico>
 {
     public void Configure(EntityTypeBuilder<Servico> builder)
     {
-        builder.ToTable("pais");
+        
+        builder.ToTable("servico"); 
+        
         builder.HasKey(s => s.IdServico);
 
-        builder.Property(s => s.IdServico).HasColumnName("id_servico").UseIdentityColumn();
-        builder.Property(s => s.NomeServico).HasColumnName("nome_servico").IsRequired().HasMaxLength(100);
-        builder.Property(s => s.Descricao).HasColumnName("descricao").HasMaxLength(500);
-
+        builder.Property(s => s.IdServico)
+            .HasColumnName("id_servico")
+            .UseIdentityColumn(); 
+            
+        builder.Property(s => s.NomeServico)
+            .HasColumnName("nome_servico")
+            .IsRequired(); 
+            
+        builder.Property(s => s.Descricao)
+            .HasColumnName("descricao"); 
     }
 }
