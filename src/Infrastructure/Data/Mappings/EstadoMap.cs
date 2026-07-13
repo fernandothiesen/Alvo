d7a215c58c2844bcf0161bd7808ef432c3f8d36b
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-13 15:14:17 -0300

    fix:mapping de estado

--- src/Infrastructure/Data/Mappings/EstadoMap.cs
+++ src/Infrastructure/Data/Mappings/EstadoMap.cs
@@ -8,10 +8,14 @@ namespace Infrastructure.Mappings;
 public class EstadoMap : IEntityTypeConfiguration<Estado>
 {
     public void Configure(EntityTypeBuilder<Estado> builder)
-    {
+    {   
+        builder.ToTable("estado");
+
         builder.HasKey(e => e.IdEstado);
-        builder.Property(e => e.NomeEstado).HasMaxLength(100).IsRequired();
-        builder.Property(e => e.SiglaEstado).IsRequired();
+
+        builder.Property(e => e.IdEstado).HasColumnName("id_estado").UseIdentityColumn();
+        builder.Property(e => e.NomeEstado).HasColumnName("nome").HasMaxLength(100).IsRequired();
+        builder.Property(e => e.SiglaEstado).HasColumnName("sigla_estado").IsRequired();
 
         builder.HasOne(e => e.Pais)
             .WithMany()
