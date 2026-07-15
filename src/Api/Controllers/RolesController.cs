6df8ba6708c1ab77936dce9dc111e77f30012ac3
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-15 14:06:17 -0300

    feat(api): controller para roles

---
+++ src/Api/Controllers/RolesController.cs
@@ -0,0 +1,34 @@
+using Application.DTOs.Role;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize] //requer autenticacao
+
+public class RolesController : ControllerBase
+{
+    private readonly IRoleService _roleService;
+
+
+    public RolesController(IRoleService roleService)
+    {
+        _roleService = roleService;
+    }
+
+    /// <summary>
+    ///    Lista todas as roles do sistema
+    
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<RoleDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> ObterTodas()
+    {
+        var roles =  await _roleService.ObterTodasAsync();
+        return Ok(roles);
+    }   
+}
