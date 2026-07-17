a7c303c2edfcea531e33114c95851d318ac1842e
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-16 21:30:53 -0300

    feat: controller de att permissoes

--- src/Api/Controllers/UsuariosController.cs
+++ src/Api/Controllers/UsuariosController.cs
@@ -1,3 +1,4 @@
+using Application.DTOs.Permissao;
 using Application.DTOs.Role;
 using Application.DTOs.Usuario;
 using Application.Interfaces;
@@ -177,6 +178,31 @@ public class UsuariosController : ControllerBase
 
         return Ok(resultado);
     }
+
+    [HttpPut("{id}/permissoes")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> AtualizarPermissoes(int id, [FromBody] AtualizarPermissaoDto dto)
+    {
+        var resultado = await _usuarioService.AtualizarPermissoesAsync(id, dto);
+
+        if(!resultado.Success)
+        {
+            if(!resultado.Success)
+            {
+                if(resultado.Message.Contains("não encontrado"))
+                {
+                    return NotFound(resultado);
+                }
+                
+                return BadRequest(resultado);
+            }
+        }
+
+        return Ok(resultado);
+    }
+
 }
     
 
