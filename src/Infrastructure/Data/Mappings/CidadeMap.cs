688357125258797fe6a3414498892cf49c2fe4ff
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-20 16:27:03 -0300

    fix(infra): nao estava mapeando cidades

--- src/Infrastructure/Data/Mappings/CidadeMap.cs
+++ src/Infrastructure/Data/Mappings/CidadeMap.cs
@@ -2,19 +2,19 @@ using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Infrastructure.Data.Configurations;
+namespace Infrastructure.Data.Mappings;
 
-public class CidadeConfiguration : IEntityTypeConfiguration<Cidade>
+public class CidadeMap : IEntityTypeConfiguration<Cidade>
 {
     public void Configure(EntityTypeBuilder<Cidade> builder)
     {
+        builder.ToTable("cidade"); // ← Isso é crucial!
         builder.HasKey(c => c.IdCidade);
 
-        builder.Property(c => c.NomeCidade).HasMaxLength(100).IsRequired();
+        builder.Property(c => c.IdCidade).HasColumnName("id_cidade").UseIdentityColumn();
+        builder.Property(c => c.IdEstado).HasColumnName("id_estado");
+        builder.Property(c => c.NomeCidade).HasColumnName("nome_cidade").IsRequired().HasMaxLength(100);
 
-        builder.HasOne(c => c.Estado)
-            .WithMany()
-            .HasForeignKey(c => c.IdEstado)
-            .IsRequired();
+        builder.HasOne(c => c.Estado).WithMany(e => e.Cidades).HasForeignKey(c => c.IdEstado);
     }
 }
