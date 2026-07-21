9c303e0ca13f0e3693b2883549a77c9767a39b26
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-21 13:15:21 -0300

    feat:adding repository do interface

--- src/Domain/Interfaces/ICidadeRepository.cs
+++ src/Domain/Interfaces/ICidadeRepository.cs
@@ -2,9 +2,9 @@ using Domain.Entities;
 
 namespace Domain.Interfaces;
 
-
 public interface ICidadeRepository : IRepository<Cidade>
 {
-    Task<IEnumerable<Cliente>> ObterAtivosAsync();
-    Task<bool> NomeExistenteAsync(string nome, int? idFornecedorExcluir = null);
+    Task<IEnumerable<Cidade>> ObterPorEstadoAsync(int idEstado);
+    Task<Cidade?> ObterPorNomeAsync(int idEstado, string nome);
+    Task<bool> NomeExisteNoEstadoAsync(int idEstado, string nome, int? idCidadeExcluir = null);
 }
