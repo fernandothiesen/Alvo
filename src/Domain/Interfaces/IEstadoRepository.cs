c5e03758599f525aeef70bead32c45448123208c
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-21 08:58:09 -0300

    feat(domain): adding interface para estado

--- src/Domain/Interfaces/IEstadoRepository.cs
+++ src/Domain/Interfaces/IEstadoRepository.cs
@@ -5,4 +5,5 @@ public interface IEstadoRepository : IRepository<Estado>
 {
     Task<IEnumerable<Estado>> ObterPaisAsync(int idPais);
     Task<Estado> ObterSiglaAsync(string sigla);
+    Task<bool> SiglaExisteNoPaisAsync(int idPais, string sigla, int? idEstadoExcluir = null);
 }
