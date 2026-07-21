66c9213ddb136b5b93ee5af20527632e2169dab5
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-21 16:13:40 -0300

    feat/mapping de demandas

--- src/Infrastructure/Data/Mappings/DemandaMap.cs
+++ src/Infrastructure/Data/Mappings/DemandaMap.cs
@@ -2,28 +2,29 @@ using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Infrastructure.Data.Configurations;
+namespace Infrastructure.Data.Mappings;
 
-public class DemandaConfiguration : IEntityTypeConfiguration<Demanda>
+public class DemandaMap : IEntityTypeConfiguration<Demanda>
 {
     public void Configure(EntityTypeBuilder<Demanda> builder)
     {
+        builder.ToTable("demanda");
         builder.HasKey(d => d.IdDemanda);
 
-        builder.Property(d => d.Titulo).HasMaxLength(200).IsRequired();
-        builder.Property(d => d.Prioridade).HasMaxLength(50);
-        builder.Property(d => d.Status).HasMaxLength(50);
+        builder.Property(d => d.IdDemanda).HasColumnName("id_demanda").UseIdentityColumn();
+        builder.Property(d => d.IdEvento).HasColumnName("id_evento");
+        builder.Property(d => d.Titulo).HasColumnName("titulo").IsRequired().HasMaxLength(200);
+        builder.Property(d => d.Descricao).HasColumnName("descricao").HasMaxLength(1000);
+        builder.Property(d => d.Prioridade).HasColumnName("prioridade").HasMaxLength(50);
+        builder.Property(d => d.Status).HasColumnName("status").HasMaxLength(50);
+        builder.Property(d => d.DataCriacao).HasColumnName("data_criacao").HasDefaultValueSql("NOW()");
+        builder.Property(d => d.DataConclusao).HasColumnName("data_conclusao");
 
-        builder.HasOne(d => d.Evento)
-            .WithMany(e => e.Demandas)
-            .HasForeignKey(d => d.IdEvento)
-            .IsRequired();
-
-        builder.Metadata.FindNavigation(nameof(Demanda.Clientes))!
-            .SetPropertyAccessMode(PropertyAccessMode.Field);
-        builder.Metadata.FindNavigation(nameof(Demanda.Fornecedores))!
-            .SetPropertyAccessMode(PropertyAccessMode.Field);
-        builder.Metadata.FindNavigation(nameof(Demanda.Financeiros))!
-            .SetPropertyAccessMode(PropertyAccessMode.Field);
+        // Relacionamentos
+        builder.HasOne(d => d.Evento).WithMany(e => e.Demandas).HasForeignKey(d => d.IdEvento);
+        
+        builder.HasMany(d => d.Clientes).WithOne(dc => dc.Demanda).HasForeignKey(dc => dc.IdDemanda);
+        builder.HasMany(d => d.Fornecedores).WithOne(df => df.Demanda).HasForeignKey(df => df.IdDemanda);
+        builder.HasMany(d => d.Financeiros).WithOne(f => f.Demanda).HasForeignKey(f => f.IdDemanda);
     }
 }
