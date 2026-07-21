c17df057b0549051d113a5d70c04cf1446eb9dd8
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-21 13:42:53 -0300

    fix: Interface de cliente repository

--- src/Domain/Interfaces/IClienteRepository.cs
+++ src/Domain/Interfaces/IClienteRepository.cs
@@ -5,5 +5,6 @@ namespace Domain.Interfaces;
 public interface IClienteRepository : IRepository<Cliente>
 {
     Task<IEnumerable<Cliente>> ObterAtivosAsync();
+    Task<Cliente?> ObterPorNomeAsync(string nome);
     Task<bool> NomeExisteAsync(string nome, int? idClienteExcluir = null);
 }
