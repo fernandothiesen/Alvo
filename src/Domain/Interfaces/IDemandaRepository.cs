eb7723ceb320f4f83745d7422275dd66684ab363
Fernando Thiesen <fernandothiesen17@gmail.com>
2026-07-21 16:08:10 -0300

    feat/demanda repository

--- src/Domain/Interfaces/IDemandaRepository.cs
+++ src/Domain/Interfaces/IDemandaRepository.cs
@@ -2,9 +2,9 @@ using Domain.Entities;
 
 namespace Domain.Interfaces;
 
-
 public interface IDemandaRepository : IRepository<Demanda>
 {
+    Task<Demanda?> ObterPorIdCompletoAsync(int idDemanda);
     Task<IEnumerable<Demanda>> ObterPorEventoAsync(int idEvento);
     Task<IEnumerable<Demanda>> ObterPorStatusAsync(string status);
 }
